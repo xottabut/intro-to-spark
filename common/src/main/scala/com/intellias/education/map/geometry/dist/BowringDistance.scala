a2bc7ff4f7fce3c0eb00d0192cf6870f00b9f44f
Anatolii Bed <ext-anatolii.bed@here.com>
2022-01-30 22:46:55 +0200

    Refactoring distance

--- common/src/main/scala/com/intellias/education/map/geometry/dist/BowringDistance.scala
+++ common/src/main/scala/com/intellias/education/map/geometry/dist/BowringDistance.scala
@@ -6,30 +6,30 @@ class BowringDistance(spheroid: Spheroid) extends Distance {
 
   override def distance(lon1: Double, lat1: Double, lon2: Double, lat2: Double): Double = {
     val a = spheroid.semiMajorAxis
-    val eprimeᵗʷᵒ = spheroid.eccentricitySquared
+    val ePrimeSq = spheroid.eccentricitySquared
 
-    val φ1 = toRadians(lat1)
-    val φ2 = toRadians(lat2)
-    val λ1 = toRadians(lon1)
-    val λ2 = toRadians(lon2)
+    val phi1 = toRadians(lat1)
+    val phi2 = toRadians(lat2)
+    val lambda1 = toRadians(lon1)
+    val lambda2 = toRadians(lon2)
 
-    val Δφ = φ2 - φ1
-    val Δλ = λ2 - λ1
+    val deltaPhi = phi2 - phi1
+    val deltaLambda = lambda2 - lambda1
 
-    val cosφ1 = cos(φ1)
-    val A = sqrt(1 + eprimeᵗʷᵒ * Math.pow(cosφ1, 4))
-    val B = sqrt(1 + eprimeᵗʷᵒ * Math.pow(cosφ1, 2))
-    val C = sqrt(1 + eprimeᵗʷᵒ)
+    val cosPhi1 = cos(phi1)
+    val A = sqrt(1 + ePrimeSq * Math.pow(cosPhi1, 4))
+    val B = sqrt(1 + ePrimeSq * Math.pow(cosPhi1, 2))
+    val C = sqrt(1 + ePrimeSq)
 
-    val w = A * Δλ / 2
-    val t1 = 3 * eprimeᵗʷᵒ * Δφ * sin(2 * φ1 + 2.0 / 3.0 * Δφ) / (4 * B * B)
-    val D = Δφ / (2 * B) * (1 + t1)
+    val w = A * deltaLambda / 2
+    val t1 = 3 * ePrimeSq * deltaPhi * sin(2 * phi1 + 2.0 / 3.0 * deltaPhi) / (4 * B * B)
+    val D = deltaPhi / (2 * B) * (1 + t1)
     val sinD = sin(D)
     val E = sinD * cos(w)
-    val t2 = B * cosφ1 * cos(D) - sin(φ1) * sinD
+    val t2 = B * cosPhi1 * cos(D) - sin(phi1) * sinD
     val F = 1 / A * sin(w) * t2
-    val σ = 2 * asin(sqrt(E * E + F * F))
-    a * C * σ / (B * B)
+    val sigma = 2 * asin(sqrt(E * E + F * F))
+    a * C * sigma / (B * B)
   }
 
 }
